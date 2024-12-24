2cd4cdd5ed2854dce15229c9304513d0eac8749e
kaveO <alban.billiette@gmail.com>
2024-12-25 00:09:31 +0100

    -_- | Merry christmas, added sort 3 digits functions when leaks when executing ft_rra function !

--- src/algorithm/sort.c
+++ src/algorithm/sort.c
@@ -6,7 +6,7 @@
 /*   By: kaveo <kaveo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 17:21:36 by kaveo             #+#    #+#             */
-/*   Updated: 2024/12/24 15:42:54 by kaveo            ###   ########.fr       */
+/*   Updated: 2024/12/25 00:01:34 by kaveo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,34 +21,30 @@ void	sort_list(t_stacks *stack_a, t_stacks *stack_b)
 	// stack_add_back(&stack_b, stack_lst_new(6));
 	if (get_list_size(stack_a) == 2)
 		ft_sa(stack_a);
-	ft_pb(&stack_b, &stack_a);
+	if (get_list_size(stack_a) == 3)
+		sort_3_digits(&stack_a);
+	// ft_pb(&stack_b, &stack_a);
+	// ft_sa(stack_a);
+	// ft_rra(&stack_a, false);
 
 	print_stack_list(stack_a);
-	ft_printf("\n");
-	print_stack_list(stack_b);
+	// ft_printf("\n");
+	// print_stack_list(stack_b);
 }
 
 void	sort_3_digits(t_stacks **stack_a)
 {
-	// TODO Sort depending of digits when there is size of three (2 instructions max)
-
-	int temp;
-
-	if ((*stack_a)->data > (*stack_a)->next->next->data)
+	if (((*stack_a)->data > (*stack_a)->next->data) &&
+		((*stack_a)->data > (*stack_a)->next->next->data))
 	{
 		ft_ra(stack_a, false);
-		return ;
+	}
+	else if ((*stack_a)->next->data > (*stack_a)->data && (*stack_a)->next->data > (*stack_a)->next->next->data)
+	{
+		ft_rra(stack_a, false);
 	}
 	if ((*stack_a)->data > (*stack_a)->next->data)
 	{
-		ft_sa((*stack_a));
-		ft_ra(stack_a, false);
+		ft_sa(*stack_a);
 	}
-	if ((*stack_a)->next->data > (*stack_a)->next->next->data)
-	{
-		temp = (*stack_a)->next->data;
-		(*stack_a)->next->data = (*stack_a)->next->next->data;
-		(*stack_a)->next->next->data = temp;
-	}
-
 }
