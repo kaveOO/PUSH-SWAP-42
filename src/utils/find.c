5b520b9bc883f307dbe605d5bcb34aff83160dbf
kaveO <alban.billiette@gmail.com>
2025-01-02 01:29:48 +0100

    Fixed error with maxint

--- src/utils/find.c
+++ src/utils/find.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   find.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaveo <kaveo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 12:57:37 by albillie          #+#    #+#             */
-/*   Updated: 2025/01/02 00:11:11 by kaveo            ###   ########.fr       */
+/*   Updated: 2025/01/02 01:28:20 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int	find_lower(t_stacks **stack_a, t_mimax *mimax)
 	t_stacks	*ptr;
 
 	ptr = (*stack_a);
-	mimax->min = 2147483648;
+	mimax->min = 2147483647;
 	if (!ptr)
 		return (0);
 	while (ptr->next)
